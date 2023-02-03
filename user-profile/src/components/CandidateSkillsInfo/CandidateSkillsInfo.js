e9e102d03c10b91e70518bcca6283542be961452
SamaraMohsin2211014KHICND <119280352+SamaraMohsin@users.noreply.github.com>
2023-02-03 16:30:08 +0500

    skill ui part done

--- user-profile/src/components/CandidateSkillsInfo/CandidateSkillsInfo.js
+++ user-profile/src/components/CandidateSkillsInfo/CandidateSkillsInfo.js
@@ -1,48 +1,104 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Heading from "../Heading/Heading";
 import styles from "../CandidateAcademicInfo/CandidateAcademicInfo.module.css";
-import Slider from '@material-ui/core/Slider'
 import InputField from "../InputField/InputField";
+import DropdownField from "../DropdownField/DropdownField";
+import Button from "../Button/Button";
+export default function CandidateskillInfo() {
 
-export default function CandidateSkillsInfo() {
-    // function valuetext(value) {
-    //     return `${value}°C`;
-    //   }
+  const[skill,setskill]= useState('');
+  const[level,SetLevel] = useState('');
+  const[skillData,SetskillData]= useState([{"skill":"java","level":"7"}]);
+  const[certificate,setCertificate] = useState([]);
+
+
+  const handleskill =useCallback ((value) => {
+    setskill(value)
+  })
+  const handleLevel = useCallback((value) => {
+    SetLevel(value);
+  })
+  const handleCertificate = useCallback((value) => {
+    setCertificate(value);
+  })
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(skillData){
+      for(let element of skillData){
+        if (element.skill === skill){
+          alert(`${skill} Already exists`);
+          setskill('');
+          SetLevel('');
+          return;
+        }
+      } 
+      SetskillData([...skillData,{skill,level}]); 
+      setskill('');
+      SetLevel('');
+    }
+    
+  }
   return (
     <div className={styles.mainContainer} style={{ display: "block" }}>
-      <form className={styles.formPersonalInfo}>
-        <Heading className={styles.personalInfoHeading} text="Skills" />{" "}
-        {/* //personalInfoHeading is name of css class */}
+      <form className={styles.formPersonalInfo} onSubmit= {onSubmit}>
+        <Heading className={styles.personalInfoHeading} text="skill" />{" "}
+        <table style={{width:"50%"}}>
+          <th>Skills</th>
+          <th>Proficiency</th>
+        {skillData.map((element,index) => {
+          return(
+            <tr key={index}>
+            <td>{element["skill"]}</td>
+            <td>{element["level"]}</td>
+          </tr>
+          )
+          })}
+
+        </table>
+        
         <InputField
-          value={""}
-          handler={""}
+          value={skill}
+          handler={handleskill}
           type="text"
-          placeholder="Skills"
+          placeholder="skill"
           className={styles.halfSize}
           required="required"
         />
-        <input  type='range' min='1' max='10' step='1' />
-        {/* <div>
-          <Slider
-            aria-label="Temperature"
-            defaultValue={30}
-            getAriaValueText={valuetext}
-            valueLabelDisplay="auto"
-            step={10}
-            marks
-            min={10}
-            max={110}
-          />
-          <Slider
-            defaultValue={30}
-            step={10}
-            marks
-            min={10}
-            max={110}
-            disabled
-          />
-        </div> */}
+        <DropdownField value={level} handler={handleLevel} options={[1,2,3,4,5,6,7,8,9,10]} placeholder ="Level" className={styles.halfSize} />
+        <Button type= 'submit' text='Add' className= {styles.saveButton} />       
       </form>
+
+      {/* ----------------xxxxxxxxxxxxx-------------------- */}
+
+      <form className={styles.formPersonalInfo} onSubmit= ''>
+        <Heading className={styles.personalInfoHeading} text="Certificates" />{" "}
+        <table style={{width:"50%"}}>
+          <th>Skills</th>
+          <th>Proficiency</th>
+        {skillData.map((element,index) => {
+          return(
+            <tr key={index}>
+            <td>{element["skill"]}</td>
+            <td>{element["level"]}</td>
+          </tr>
+          )
+          })}
+
+        </table>
+        
+        <InputField
+          value={certificate}
+          handler={handleCertificate}
+          type="file"
+          placeholder=""
+          className={styles.halfSize}
+        />
+        <DropdownField value={level} handler={handleLevel} options={[1,2,3,4,5,6,7,8,9,10]} placeholder ="Level" className={styles.halfSize} />
+        <Button type= 'submit' text='Add' className= {styles.saveButton} />       
+      </form>
+
     </div>
   );
 }
