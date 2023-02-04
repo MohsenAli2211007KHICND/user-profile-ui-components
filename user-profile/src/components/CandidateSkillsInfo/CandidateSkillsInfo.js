2939f1e774166f953eeba797795157a35bad7615
Hafiza-Samara-Mohsin <mohsin4005617@cloud.neduet.edu.pk>
2023-02-04 23:51:00 +0500

    upload file feature added

--- user-profile/src/components/CandidateSkillsInfo/CandidateSkillsInfo.js
+++ user-profile/src/components/CandidateSkillsInfo/CandidateSkillsInfo.js
@@ -5,59 +5,71 @@ import InputField from "../InputField/InputField";
 import DropdownField from "../DropdownField/DropdownField";
 import Button from "../Button/Button";
 export default function CandidateskillInfo() {
+  const [skill, setskill] = useState("");
+  const [level, SetLevel] = useState("");
+  const [skillData, SetskillData] = useState([{ skill: "java", level: "7" }]);
+  const [certificate, setCertificate] = useState();
+  const [certificateData, setCertificateData] = useState([]);
 
-  const[skill,setskill]= useState('');
-  const[level,SetLevel] = useState('');
-  const[skillData,SetskillData]= useState([{"skill":"java","level":"7"}]);
-  const[certificate,setCertificate] = useState([]);
-
-
-  const handleskill =useCallback ((value) => {
-    setskill(value)
-  })
+  const handleskill = useCallback((value) => {
+    setskill(value);
+  });
   const handleLevel = useCallback((value) => {
     SetLevel(value);
-  })
+  });
   const handleCertificate = useCallback((value) => {
     setCertificate(value);
-  })
-
+  });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    if(skillData){
-      for(let element of skillData){
-        if (element.skill === skill){
+    if (skillData) {
+      for (let element of skillData) {
+        if (element.skill === skill) {
           alert(`${skill} Already exists`);
-          setskill('');
-          SetLevel('');
+          setskill("");
+          SetLevel("");
           return;
         }
-      } 
-      SetskillData([...skillData,{skill,level}]); 
-      setskill('');
-      SetLevel('');
+      }
+      SetskillData([...skillData, { skill, level }]);
+      setskill("");
+      SetLevel("");
     }
-    
-  }
+  };
+
+  const upload = (e) => {
+    e.preventDefault();
+    if (certificateData) {
+      for (let element of certificateData) {
+        if (element.name === certificate.name) {
+          alert(`${certificate} Already exist`);
+          setCertificate("");
+          return;
+        }
+      }
+      setCertificateData([...certificateData, certificate]);
+      setCertificate("");
+    }
+    // console.log(certificate);
+    console.log(certificateData);
+  };
   return (
     <div className={styles.mainContainer} style={{ display: "block" }}>
-      <form className={styles.formPersonalInfo} onSubmit= {onSubmit}>
+      <form className={styles.formPersonalInfo} onSubmit={onSubmit}>
         <Heading className={styles.personalInfoHeading} text="skill" />{" "}
-        <table style={{width:"50%"}}>
+        <table style={{ width: "50%" }}>
           <th>Skills</th>
           <th>Proficiency</th>
-        {skillData.map((element,index) => {
-          return(
-            <tr key={index}>
-            <td>{element["skill"]}</td>
-            <td>{element["level"]}</td>
-          </tr>
-          )
+          {skillData.map((element, index) => {
+            return (
+              <tr key={index}>
+                <td>{element["skill"]}</td>
+                <td>{element["level"]}</td>
+              </tr>
+            );
           })}
-
         </table>
-        
         <InputField
           value={skill}
           handler={handleskill}
@@ -66,39 +78,40 @@ export default function CandidateskillInfo() {
           className={styles.halfSize}
           required="required"
         />
-        <DropdownField value={level} handler={handleLevel} options={[1,2,3,4,5,6,7,8,9,10]} placeholder ="Level" className={styles.halfSize} />
-        <Button type= 'submit' text='Add' className= {styles.saveButton} />       
+        <DropdownField
+          value={level}
+          handler={handleLevel}
+          options={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+          placeholder="Level"
+          className={styles.halfSize}
+        />
+        <Button type="submit" text="Add" className={styles.saveButton} />
       </form>
 
       {/* ----------------xxxxxxxxxxxxx-------------------- */}
 
-      <form className={styles.formPersonalInfo} onSubmit= ''>
+      <form className={styles.formPersonalInfo} onSubmit={upload}>
         <Heading className={styles.personalInfoHeading} text="Certificates" />{" "}
-        <table style={{width:"50%"}}>
-          <th>Skills</th>
-          <th>Proficiency</th>
-        {skillData.map((element,index) => {
-          return(
-            <tr key={index}>
-            <td>{element["skill"]}</td>
-            <td>{element["level"]}</td>
-          </tr>
-          )
+        <table style={{ width: "50%" }}>
+          <th>Certificates</th>
+          {certificateData.map((element, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{element.name}</td>
+              </tr>
+            );
           })}
-
         </table>
-        
         <InputField
-          value={certificate}
+          // value={certificate.name}
           handler={handleCertificate}
           type="file"
           placeholder=""
           className={styles.halfSize}
         />
-        <DropdownField value={level} handler={handleLevel} options={[1,2,3,4,5,6,7,8,9,10]} placeholder ="Level" className={styles.halfSize} />
-        <Button type= 'submit' text='Add' className= {styles.saveButton} />       
+        <Button type="submit" text="Upload" className={styles.saveButton} />
       </form>
-
     </div>
   );
 }
--- user-profile/src/components/InputField/InputField.js
+++ user-profile/src/components/InputField/InputField.js
@@ -1,28 +1,44 @@
-import styles from '../CandidatePersonalInfo/CandidatePersonalInfo.module.css';
+import styles from "../CandidatePersonalInfo/CandidatePersonalInfo.module.css";
 
-function InputField({ type, placeholder, className, required, icon, value, handler, disabled, pattern, min }) {
-
-    const handleChange = (e) => {
-        let value = e.target.validity.valid ? e.target.value : e.target.value.substr(0, e.target.value.length - 1);
-        handler(value)
+function InputField({
+  type,
+  placeholder,
+  className,
+  required,
+  icon,
+  value,
+  handler,
+  disabled,
+  pattern,
+  min,
+}) {
+  const handleChange = (e) => {
+    let value;
+    if (type === "file") {
+      value = e.target.files[0];
+    } else {
+      value = e.target.validity.valid
+        ? e.target.value
+        : e.target.value.substr(0, e.target.value.length - 1);
     }
+    handler(value);
+  };
 
-    return (
-        <>
-            <input
-                min={min}
-                pattern={pattern}
-                disabled={disabled}
-                value={value}
-                onChange={handleChange}
-                type={type}
-                placeholder={placeholder}
-                className={`${className} ${styles.inputFields}`}
-                required={required}
-            />
-
-        </>
-    )
+  return (
+    <>
+      <input
+        min={min}
+        pattern={pattern}
+        disabled={disabled}
+        value={value}
+        onChange={handleChange}
+        type={type}
+        placeholder={placeholder}
+        className={`${className} ${styles.inputFields}`}
+        required={required}
+      />
+    </>
+  );
 }
 
 export default InputField;
