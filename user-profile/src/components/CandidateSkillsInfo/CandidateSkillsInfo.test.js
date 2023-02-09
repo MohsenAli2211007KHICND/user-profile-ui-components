d45d8f35c0725385a31a164fe7c5bae0ba0bcbff
MohsenAli2211007KHICND <mohsen.ali@xloopdigital.com>
2023-02-09 12:55:06 +0500

    Refactor test  file

--- user-profile/src/components/CandidateSkillsInfo/CandidateSkillsInfo.test.js
+++ user-profile/src/components/CandidateSkillsInfo/CandidateSkillsInfo.test.js
@@ -13,24 +13,25 @@ describe('CandidateSkillsInfo component test', () => {
             status: 200
         })
     });
-        // **************************************** Skills ****************************************
+    // **************************************** Skills ****************************************
 
     it('should get user Skill Data on first render', async () => {
         render(<CandidateskillInfo />);
         expect(fetch).toHaveBeenCalledTimes(2);
-        const {skill, proficiency} = mockData[0]
+        const { skill, proficiency } = mockData[0]
         await waitFor(() => {
             const sk = screen.getAllByText(skill);
             const lv = screen.getAllByText(proficiency)
-            sk.forEach((ele) =>{
+            sk.forEach((ele) => {
                 expect(ele).toBeInTheDocument();
             });
             lv.forEach((ele) => {
                 expect(ele).toBeInTheDocument();
             })
         })
-        
+
     });
+    // ********************************************************************************
 
     it('should be able to add new skill details when we click Add button', () => {
         render(<CandidateskillInfo />);
@@ -39,13 +40,14 @@ describe('CandidateSkillsInfo component test', () => {
             bubbles: true,
             cancelable: true
         }));
-     waitFor(() => {
+        waitFor(() => {
             expect(screen.getByPlaceholderText('skill')).toBeInTheDocument();
         })
-         waitFor(() => {
+        waitFor(() => {
             expect(screen.getByText('level')).toBeInTheDocument();
         })
     });
+    // ********************************************************************************
 
     it('text can be inputted into the Skill field', () => {
         render(<CandidateskillInfo />);
@@ -62,9 +64,10 @@ describe('CandidateSkillsInfo component test', () => {
         })
         fireEvent.change(levelInput, { target: { value: '5' } })
         expect(levelInput.value).toBe('5')
-      });
+    });
+    // ********************************************************************************
 
-      it('should send post request on clicking Add button', () => {
+    it('should send post request on clicking Add button', () => {
         render(<CandidateskillInfo />);
         const addBtn = screen.getByText("Add");
         fireEvent(addBtn, new MouseEvent('click', {
@@ -72,7 +75,7 @@ describe('CandidateSkillsInfo component test', () => {
             cancelable: true
         }));
         let addButton;
-         waitFor(() => {
+        waitFor(() => {
             addButton = screen.getByText("Add")
         })
         fireEvent(addButton, new MouseEvent('click', {
@@ -86,19 +89,21 @@ describe('CandidateSkillsInfo component test', () => {
     it('should get user Certificate Data on first render', async () => {
         render(<CandidateskillInfo />);
         expect(fetch).toHaveBeenCalledTimes(2);
-        const { originalFileName, category} = mockData[1]
+        const { originalFileName, category } = mockData[1]
         await waitFor(() => {
             const cert = screen.getAllByText(originalFileName);
             const cat = screen.getAllByText(category)
-            cert.forEach((ele) =>{
+            cert.forEach((ele) => {
                 expect(ele).toBeInTheDocument();
             });
             cat.forEach((ele) => {
                 expect(ele).toBeInTheDocument();
             })
         })
-        
+
     });
+    // ********************************************************************************
+
     it('should be able to add new Certificate when we click Add button', () => {
         render(<CandidateskillInfo />);
         const uploadBtn = screen.getByText("Upload");
@@ -106,13 +111,14 @@ describe('CandidateSkillsInfo component test', () => {
             bubbles: true,
             cancelable: true
         }));
-     waitFor(() => {
+        waitFor(() => {
             expect(screen.getByText('No file selected.')).toBeInTheDocument();
         })
-         waitFor(() => {
+        waitFor(() => {
             expect(screen.getByText('Category')).toBeInTheDocument();
         })
     });
+    // ********************************************************************************
 
     it('file should be selected in inputted of the certificate field', () => {
         render(<CandidateskillInfo />);
@@ -120,7 +126,7 @@ describe('CandidateSkillsInfo component test', () => {
         let certInput;
         waitFor(() => {
             certInput = screen.getByPlaceholderText("Certificate")
-        }) 
+        })
         const file = new File([""], "Build+Your+API+with+Spring.pdf", { type: "application/pdf" });
         fireEvent.change(certInput, { target: { files: [file] } });
         expect(certInput.files[0].name).toBe('Build+Your+API+with+Spring.pdf')
@@ -130,9 +136,10 @@ describe('CandidateSkillsInfo component test', () => {
         })
         fireEvent.change(catInput, { target: { value: 'Other' } })
         expect(catInput.value).toBe('Other')
-      });
+    });
+    // ********************************************************************************
 
-      it('should send post request on clicking Add button', () => {
+    it('should send post request on clicking Add button', () => {
         render(<CandidateskillInfo />);
         const uploadBtn = screen.getByText("Upload");
         fireEvent(uploadBtn, new MouseEvent('click', {
