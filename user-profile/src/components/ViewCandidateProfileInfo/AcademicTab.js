9c9c6cd8803d06194dd2b55a088d7cd2e0fb08aa
SamaraMohsin2211014KHICND <119280352+SamaraMohsin@users.noreply.github.com>
2023-02-09 16:40:18 +0500

    acdemic info data fatching

--- user-profile/src/components/ViewCandidateProfileInfo/AcademicTab.js
+++ user-profile/src/components/ViewCandidateProfileInfo/AcademicTab.js
@@ -1,11 +1,55 @@
-import React from 'react'
-import Heading from '../Heading/Heading'
+import React, { useEffect, useState } from "react";
+import Heading from "../Heading/Heading";
+import styles from "../CandidatePersonalInfo/CandidatePersonalInfo.module.css";
 
 export default function AcademicTab() {
+  const [acdemicData, setAcademicData] = useState("");
+
+  useEffect(() => {
+    const academicInfo = () => {
+      fetch("http://192.168.0.160:8080/api/educational_information")
+        .then(async (response) => {
+          console.log(response);
+          const data = await response.json;
+          console.log(data);
+          setAcademicData(data);
+        })
+        .catch((err) => {
+          console.log("Hello I caught this error!");
+        });
+    };
+    academicInfo();
+  },[]);
+
   return (
     <div>
-        <Heading text="Academic Information" className= {personalInfoHeading}/>
-
+      <Heading
+        text="Academic Information"
+        className={styles.personalInfoHeading}
+      />
+      <div>
+        <table border={2}>
+          <tr>
+            <td>Degree : </td>
+            <td>{}</td>
+            <td>Degree Title : </td>
+            <td>{}</td>
+          </tr>
+          <tr>
+            <td>Graduation Date : </td>
+            <td>{}</td>
+            <td>CGPA : </td>
+            <td>{}</td>
+          </tr>
+          <tr>
+            <td>Institute/University : </td>
+            <td>{}</td>
+            {/* <td>Degree In Progress</td> */}
+            <td>Final Year Project : </td>
+            <td>{}</td>
+          </tr>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
