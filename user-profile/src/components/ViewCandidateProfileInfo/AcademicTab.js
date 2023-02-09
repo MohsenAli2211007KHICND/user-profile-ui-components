7d4232f40e47ec225b3ef4cef0f5e47df80da9ae
SamaraMohsin2211014KHICND <119280352+SamaraMohsin@users.noreply.github.com>
2023-02-09 14:10:59 +0500

    Academic Tab file created

---
+++ user-profile/src/components/ViewCandidateProfileInfo/AcademicTab.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+export default function AcademicTab() {
+  return (
+    <div>
+        <Heading text="Academic Information" className= {personalInfoHeading}/>
+
+    </div>
+  )
+}
--- user-profile/src/components/ViewCandidateProfileInfo/ViewCandidateProfileInfo.js
+++ user-profile/src/components/ViewCandidateProfileInfo/ViewCandidateProfileInfo.js
@@ -4,6 +4,7 @@ export default function ViewCandidateProfileInfo() {
   return (
     <div>
       
+      <AcademicTab/>
     </div>
   )
 }
